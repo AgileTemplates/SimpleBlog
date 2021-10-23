7a5e2592af0af48450f2ad7835d10070e0a947b9
Dom Vinyard <domvinyard@me.com>
2021-10-23 17:40:56 +0200

    Create get-post.js

---
+++ src/server/get-post.js
@@ -0,0 +1,26 @@
+import { Pool } from 'pg';
+
+export async function handler(req) {
+  // Connect to database
+  const ssl = { rejectUnauthorized: false };
+  const connectionString = process.env.DATABASE_URL;
+  const database = new Pool({ connectionString, ssl });
+  const { id } = JSON.parse(req.body);
+
+  try {
+    // Add a new post
+    await database.query(`SELECT * FROM posts where id = $1`, [id]);
+
+    // End the connection and return a success (200) response
+    await database.end();
+    return { statusCode: 200 };
+
+    // If error, return error
+  } catch (err) {
+    await database.end();
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+}
